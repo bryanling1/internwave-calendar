e669688565d41b004da6ce4eb0f5fb962275be0b
bryanling1 <bryanling107@gmail.com>
2024-10-21 19:23:25 -0400

    Add readme and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## v1.0.0
+
+### Added or Changed
+- Added ability to add notes from jobs table
+- Added start and end date inputs
+- Added list to sort events by start and end date on a given note
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs"], // Build for commonJS and ESmodules
   splitting: false,
-  outDir: "build",
+  outDir: "G:/uni/uwwave/code/client/desktop/public/extensions/calendar",
+  noExternal: [ /(.*)/ ],
+  target: "node18"
 });
